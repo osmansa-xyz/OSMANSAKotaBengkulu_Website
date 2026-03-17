f723e056da613986cc13881ca3947202ec4720c4
osmansa-xyz <osmansakotabengkulu@gmail.com>
2026-03-17 16:32:53 +0700

    Update database setup with new event entries
    Add new event data and print confirmation message

--- database.py
+++ database.py
@@ -100,7 +100,8 @@ def setup_event():
         ('Isra Miraj', 'isra.jpg', 'Peringatan perjalanan suci Nabi Muhammad SAW yang diisi dengan kajian mendalam untuk meningkatkan keimanan dan ketakwaan siswa.'),
         ('Bulan Bahasa', 'bahasa.jpg', 'Perayaan untuk mengapresiasi kekayaan bahasa dan sastra melalui berbagai lomba kreatif seperti ranking 1, dongeng, dan sayembara maskot.'),
         ('Liga MAN', 'liga.jpg', 'Kompetisi olahraga tahunan antarkelas yang dirancang untuk membangun sportivitas, kerjasama tim, dan kesehatan fisik siswa.'),
-        ('Maulid Nabi', 'maulid.jpg', 'Perayaan hari lahir Nabi Muhammad SAW yang dirayakan dengan penuh syukur melalui lantunan shalawat, tausiyah, serta aksi sosial.')
+        ('Maulid Nabi', 'maulid.jpg', 'Perayaan hari lahir Nabi Muhammad SAW yang dirayakan dengan penuh syukur melalui lantunan shalawat, tausiyah, serta aksi sosial.'),
+        ('Social Project Ramadan: MABAR#1 (Madrasah Berburu Amal Ramadan)', 'spr.jpg', 'Proyek sosial mandiri OSIM MAN 1 Kota Bengkulu yang diisi dengan penyaluran bantuan ke panti asuhan dan pembagian takjil gratis di sekitar lingkungan madrasah selama bulan Ramadan.')
     ]
     cursor.executemany("INSERT INTO event (nama_event, foto, deskripsi) VALUES (?, ?, ?)", event_data)
     
